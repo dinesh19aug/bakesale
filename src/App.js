f052daefcc4e5d021dfebe31a0430cb160f5833e
dinesh19aug@gmail.com <dinesh19aug@gmail.com>
2018-05-06 21:40:47 -0400

    Code Set up for Bakesale

---
+++ src/App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={[ styles.header]}> BakeSale </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header:{
+    fontSize: 40
+  },
+});
